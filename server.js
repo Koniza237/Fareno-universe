6cd04470ccfb77dfefca4cce90d7821604db2038
Koniza237 <konizakoniza3@gmail.com>
2025-06-08 03:54:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -668,5 +668,5 @@ app.use((req, res, next) => {
     res.status(404).json({ error: 'Route non trouvée' });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur démarré sur http://localhost:${PORT}`));
